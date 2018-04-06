90a18a3b2bbaf5ea0f73b70a0693a0b12cd65b0f
libo-szu <33837403+libo-szu@users.noreply.github.com>
2018-04-06 14:41:10 +0800

    Update new_unet.py

--- new_unet.py
+++ new_unet.py
@@ -1,8 +1,23 @@
 
+from __future__ import print_function
+import numpy as np
+from keras.models import Model,load_model
+from keras.layers import Input, merge
+from keras.layers.convolutional import Conv2D,UpSampling2D
+from keras.layers.pooling  import MaxPooling2D
+from keras.optimizers import Adam
+from keras.callbacks import ModelCheckpoint, LearningRateScheduler
+from data import load_train_data, load_test_data
+
+
+import matplotlib.pyplot as plt
+from train_test import dice_coef_loss,dice_coef
+
+
+from keras import backend as K
+K.set_image_dim_ordering('th')
+
 """
-
-
-
 new_unet:先三分支后三分支融合形成第四分支，后融合形成conv10卷积层
 
 
